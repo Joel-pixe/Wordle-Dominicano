8213085c9a81927d397397c2918a3be020378a55
kou-07 <papitafrita666@gmail.com>
2026-03-11 15:30:59 -0400

    prueba

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("scarlet");
+
+
+
+
+            Console.WriteLine('a');
+            Console.ReadKey();
+        }
+    }
+}
