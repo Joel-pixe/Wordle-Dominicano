e59666a585682644af1e0475e125b18ad252c8bd
kou-07 <666papitasfritas@gmail.com>
2026-03-11 15:43:24 -0400

    Add files via upload
    mmg

---
+++ WORDLE CODE/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp1
+{
+    internal class Wordle
+    {
+        new class JuegoWORDLE
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            
+        
+        
+        }
+    }
+}
