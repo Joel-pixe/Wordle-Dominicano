89dc9c9ba05bc30ec7e0065470b27766fb4b779a
Joel-pixe <70036255+Joel-pixe@users.noreply.github.com>
2026-03-14 14:14:57 -0400

    Joel commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp1
+{
+    internal class Wordle
+    {
+        new class JuegoWORDLE
+        {
+            Console.WriteLine("Hello Soy Joel");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola")
+        }
+    }
+}
