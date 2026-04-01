5a5e7d0f970d4df5d07ed2c7ba1b0fe670e3e1ff
kou-07 <papitafrita666@gmail.com>
2026-03-31 22:38:11 -0400

    mensaje inicial clase juego wordle

--- Program.cs
+++ Program.cs
@@ -2,11 +2,30 @@
 {
     internal class WordleDominicano
     {
+        public class JuegoWordle
+        {
+            public string palabraSecreta { get; }
+            public int intentosMaximos = 6;
+
+            public void MsgInicial()
+            {
+                Console.WriteLine("¡Bienvenido a WordleMMG!");
+                Console.WriteLine($"Adivina la palabra de {palabraSecreta.Length} letras en {intentosMaximos} intentos.");
+                Console.WriteLine("Después de cada intento, recibirás estas pistas:");
+                Console.WriteLine("- Letra en verde: letra correcta en la posición correcta.");
+                Console.WriteLine("- Letra en amarillo: letra correcta en la posición incorrecta.");
+                Console.WriteLine("- Letra en gris: letra no está en la palabra.");
+            }
+
+            public void Juego()
+            {
+
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.WriteLine("Joel hablar mucho");
-            Console.WriteLine("Joel es un nino rata");
+            
         }
     }
 }
