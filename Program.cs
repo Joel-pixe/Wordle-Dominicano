5016bbedf7a81679254e79b6e936f242492c9b53
kou-07 <papitafrita666@gmail.com>
2026-03-31 22:09:37 -0400

    HOLA CHICOS

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@
         {
             Console.WriteLine("Hello World");
             Console.WriteLine("Joel hablar mucho");
+            Console.WriteLine("Joel es un nino rata");
         }
     }
 }
